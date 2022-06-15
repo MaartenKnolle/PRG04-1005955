9059d3c7ba7a47ecdd1296e377775c6290cd0f02
Maarten Knolle <maarten.knolle@gmail.com>
2022-06-16 00:00:02 +0200

    Item spawning behaviour V1
    spawns in 3 lanes.  drops after 2 seconds. switches lane every 10 seconds

--- src/game.ts
+++ src/game.ts
@@ -1,20 +1,26 @@
 import * as PIXI from "pixi.js";
 
 import raccoonImage from "./images/raccoon_climbing_2.png";
+import bottleImage from "./images/bottle_broken.png";
 import backgroundImage from "./images/background.png";
 import { Player } from "./Player";
-import { Item } from "./Item";
+import { Item } from "./item";
+import { UI } from "./ui";
 
-class Game {
-  pixi: PIXI.Application;
-  loader: PIXI.Loader;
-  raccoon: Player;
-  item: Item;
+export class Game {
+  private pixi: PIXI.Application;
+  private loader: PIXI.Loader;
+  private raccoon: Player;
+  private items: Item[] = [];
+  private time: number = 0;
+  private interface: UI;
 
   constructor() {
     this.pixi = new PIXI.Application({
       width: window.innerWidth,
       height: window.innerHeight,
+      resolution: window.devicePixelRatio,
+      autoDensity: true,
     });
     document.body.appendChild(this.pixi.view);
 
@@ -24,30 +30,87 @@ class Game {
     this.pixi.stage.addChild(background);
 
     this.loader = new PIXI.Loader();
-    this.loader.add("raccoonImage", raccoonImage);
+    this.loader
+      .add("raccoonImage", raccoonImage)
+      .add("bottleImage", bottleImage);
 
     this.loader.load(() => this.loadCompleted());
   }
 
-  loadCompleted() {
-    {
-      this.raccoon = new Player(this.loader.resources["raccoonImage"].texture!);
-      this.raccoon.scale.set(0.25);
-      this.raccoon.anchor.set(0.5, 0.5);
-      this.pixi.stage.addChild(this.raccoon);
-      console.log(this.raccoon);
-    }
-    {
-      this.item = new Item(this.loader.resources["raccoonImage"].texture!);
-      this.item.scale.set(0.25);
-      this.pixi.stage.addChild(this.item);
-    }
+  private loadCompleted() {
+    this.raccoon = new Player(this.loader.resources["raccoonImage"].texture!);
+    this.raccoon.scale.set(0.25);
+    this.raccoon.anchor.set(0.5, 0.5);
+    this.pixi.stage.addChild(this.raccoon);
+
+    this.interface = new UI();
+    this.pixi.stage.addChild(this.interface);
 
     this.pixi.ticker.add((delta) => this.update(delta));
   }
-  update(delta: number) {
+  private update(delta: number) {
+    this.time++;
+
+    this.time = Math.floor(this.time);
+
     this.raccoon.update();
-    this.item.update();
+
+    for (let item of this.items) {
+      item.update(delta);
+    }
+
+    switch (this.time) {
+      case 600:
+        console.log(`left`);
+
+        for (var i = 0; i < 5; i++) {
+          let item = new Item(
+            this.loader.resources["bottleImage"].texture!,
+            this,
+            0,
+            window.innerWidth / 3
+          );
+          item.scale.set(0.2);
+          this.items.push(item);
+          this.pixi.stage.addChild(item);
+        }
+        break;
+      case 1200:
+        console.log(`middle`);
+
+        for (var i = 0; i < 5; i++) {
+          let item = new Item(
+            this.loader.resources["bottleImage"].texture!,
+            this,
+            window.innerWidth / 3,
+            (window.innerWidth / 3) * 2
+          );
+          item.scale.set(0.2);
+          this.items.push(item);
+          this.pixi.stage.addChild(item);
+        }
+        break;
+      case 1800:
+        console.log(`right`);
+
+        for (var i = 0; i < 5; i++) {
+          let item = new Item(
+            this.loader.resources["bottleImage"].texture!,
+            this,
+            (window.innerWidth / 3) * 2,
+            window.innerWidth
+          );
+          item.scale.set(0.2);
+          this.items.push(item);
+          this.pixi.stage.addChild(item);
+        }
+        this.time = 0;
+        break;
+    }
+  }
+  public removeItem(itemToRemove: Item) {
+    this.items = this.items.filter((item: Item) => item != itemToRemove);
+    this.pixi.stage.removeChild(itemToRemove);
   }
 }
 let game = new Game();
--- src/item.ts
+++ src/item.ts
@@ -1,21 +1,40 @@
 import * as PIXI from "pixi.js";
+import { Game } from "./game";
 
 export class Item extends PIXI.Sprite {
-  private yspeed = 5;
+  private yspeed = 0;
+  private time = 0;
+  private game: Game;
+  private canMove = false;
 
-  constructor(texture: PIXI.Texture) {
+  constructor(texture: PIXI.Texture, game: Game, minX: number, maxX: number) {
     super(texture);
+    this.game = game;
+    this.yspeed = Math.random() * 8;
+    if (this.yspeed <= 4) {
+      this.yspeed + 4;
+    }
     this.anchor.set(0.5);
-    this.x = Math.random() * window.innerWidth;
-    this.y = 0;
+    this.y = Math.random() * window.innerHeight;
+    this.x = this.randomIntFromInterval(minX, maxX);
   }
-  update() {
-    this.y += this.yspeed;
+  update(delta: number) {
+    this.time += delta;
+    if (this.time >= 120) {
+      this.canMove = true;
+      //this.wiggle();
+    }
+    if (this.canMove === true) {
+      this.y += this.yspeed;
+    }
 
     //check bottom
-    if (this.y >= window.innerHeight) {
-      this.x = Math.random() * window.innerWidth;
-      this.y = 0;
-    }
+    if (this.y > 1000) this.game.removeItem(this);
   }
+
+  randomIntFromInterval(min: number, max: number) {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  wiggle() {}
 }
